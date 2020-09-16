3dc12b19a34f053dd2466dd43a53d3ae1c1d0f74
MahlonaVitalii <maglona2013@gmail.com>
2020-09-16 22:07:42 +0300

    removed unused main page locator

--- pages/locators.py
+++ pages/locators.py
@@ -1,14 +1,10 @@
 from selenium.webdriver.common.by import By
 
-
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     BASKET_LINK = (By.CSS_SELECTOR, ".basket-mini a")
     USER_ICON = (By.CSS_SELECTOR, ".icon-user")
-	
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 	
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
