2f2041bcc13aa0890aeac5329cacda22d91307ae
MahlonaVitalii <maglona2013@gmail.com>
2020-09-16 11:32:53 +0300

    added locators for ProductPage

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,11 @@ class MainPageLocators():
 class LoginPageLocators():
 	LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
 	REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+	
+class ProductPageLocators():
+    ADD_TO_BACKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    ADDED_PRODUCT_MESSAGE = (By.CSS_SELECTOR, ".alert-success:nth-child(1) strong")
+    BACKET_PRICE_MESSAGE = (By.CSS_SELECTOR, "div.basket-mini")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main > h1")
+	
