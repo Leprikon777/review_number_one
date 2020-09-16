c6599ba493aca6116c537dff1aa40c00d5cbe1b6
MahlonaVitalii <maglona2013@gmail.com>
2020-09-16 21:55:05 +0300

    removed empty line

--- test_main_page.py
+++ test_main_page.py
@@ -20,7 +20,6 @@ class TestLoginFromMainPage():
         page.open()
         page.should_be_login_link()	
 	
-	
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     page = MainPage(browser, link)   
     page.open()
