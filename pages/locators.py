b06010afa75f0726d6d1dbcffe35d70d323ac9b1
MahlonaVitalii <maglona2013@gmail.com>
2020-09-16 20:54:14 +0300

    modified locators (for registration)

--- pages/locators.py
+++ pages/locators.py
@@ -5,13 +5,18 @@ class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     BASKET_LINK = (By.CSS_SELECTOR, ".basket-mini a")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 	
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 	
 class LoginPageLocators():
-	LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
-	REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    EMAIL_INPUT = (By.CSS_SELECTOR, "[name='registration-email']")
+    PASSWORD_INPUT = (By.CSS_SELECTOR, "[name='registration-password1']")
+    CONFIRM_PASSWORD_INPUT = (By.CSS_SELECTOR, "[name='registration-password2']")
+    REGISTRATION_SUBMIT = (By.CSS_SELECTOR, "[name='registration_submit']")
 	
 class ProductPageLocators():
     ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
