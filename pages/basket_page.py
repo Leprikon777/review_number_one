3b4fd758595fa8a9e2ec13cc6a421e7f9e470e18
MahlonaVitalii <maglona2013@gmail.com>
2020-09-16 22:05:26 +0300

    updated messages for basket checks

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -4,8 +4,8 @@ from .locators import BasketPageLocators
 class BasketPage(BasePage):
     def should_be_message_basket_is_empty(self):
         assert self.is_element_present(*BasketPageLocators.BASKET_IS_EMPTY_MESSAGE), \
-            "'The basket is empty' message is not presented on basket page"
+            "The message that basket is empty is not presented on basket page"
 	
     def is_not_basket_contains_products(self):
         assert self.is_not_element_present(*BasketPageLocators.BASKET_TITLE), \
-            "The basket contains products"
+            "There are products in the basket, but it shouldn't be"
