bfd8a0f470c09229b44fe3de86691f1281b54ace
MahlonaVitalii <maglona2013@gmail.com>
2020-09-16 17:38:10 +0300

    added several tests and helper functions

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,11 +1,14 @@
 from .base_page import BasePage
 from .locators import ProductPageLocators
 
-class ProductPage(BasePage): 
-    def should_be_correct_adding_product_to_basket(self):
+class ProductPage(BasePage):
+    def add_product_to_basket(self):
         add_to_basket_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
         add_to_basket_button.click()
-        self.solve_quiz_and_get_code()
+        self.solve_quiz_and_get_code()	
+		
+    def should_be_correct_adding_product_to_basket(self):
+        self.add_product_to_basket()
 		
         self.should_be_message_about_added_product()
         self.should_be_message_about_current_product()
@@ -13,17 +16,29 @@ class ProductPage(BasePage):
         self.should_be_price_basket_equals_current_product()
 		
     def should_be_message_about_added_product(self):
-        assert self.is_element_present(*ProductPageLocators.ADDED_PRODUCT_MESSAGE), "Message about added product is not presented"
+        assert self.is_element_present(*ProductPageLocators.ADDED_PRODUCT_MESSAGE), \
+            "Message about added product is not presented"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.ADDED_PRODUCT_MESSAGE), \
+           "Message about added product is presented, but should not be"
+
+    def should_be_success_message_is_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.ADDED_PRODUCT_MESSAGE), \
+           "Message about added product is not disappeared"
 
     def should_be_message_about_price_basket(self):
-        assert self.is_element_present(*ProductPageLocators.BASKET_PRICE_MESSAGE), "Message about price of the basket is not presented"
+        assert self.is_element_present(*ProductPageLocators.BASKET_PRICE_MESSAGE), \
+            "Message about price of the basket is not presented"
 
     def should_be_message_about_current_product(self):
         message = self.get_text_of_element(*ProductPageLocators.ADDED_PRODUCT_MESSAGE)
         product_name = self.get_text_of_element(*ProductPageLocators.PRODUCT_NAME)
-        assert self.is_equals(message, product_name), "The name of the product in the message doesn't match the product that was added"
+        assert self.is_equals(message, product_name), \
+            "The name of the product in the message doesn't match the product that was added"
 		
     def should_be_price_basket_equals_current_product(self):
         bucket_price = self.get_text_of_element(*ProductPageLocators.BASKET_PRICE_MESSAGE)
         product_price = self.get_text_of_element(*ProductPageLocators.PRODUCT_PRICE)
-        assert self.is_main_message_contains_sub(bucket_price, product_price), "Basket price doesn't match the product price"
+        assert self.is_main_message_contains_sub(bucket_price, product_price), \
+            "Basket price doesn't match the product price"
--- test_product_page.py
+++ test_product_page.py
@@ -2,7 +2,7 @@ from .pages.product_page import ProductPage
 import pytest
 
 # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-# link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
+link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -18,3 +18,28 @@ def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)   
     page.open()                
     page.should_be_correct_adding_product_to_basket()
+	
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link)   
+    page.open()               
+    page.add_product_to_basket()
+    page.should_not_be_success_message()
+	
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, link)   
+    page.open()               
+    page.should_not_be_success_message()
+	
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link)   
+    page.open()               
+    page.add_product_to_basket()
+    page.should_be_success_message_is_disappeared()
+	
+	
+	
+	
+	
+	
+	
+	
