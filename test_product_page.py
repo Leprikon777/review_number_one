4f925d1f0bf017b522966e24d703ad4810667823
MahlonaVitalii <maglona2013@gmail.com>
2020-09-16 20:56:55 +0300

    created TestUserAddToBasketFromProductPage class

--- test_product_page.py
+++ test_product_page.py
@@ -1,36 +1,42 @@
 from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
+from .pages.login_page import LoginPage
 
 import pytest
+import time
 
 # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
 link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
 
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def test_guest_can_add_product_to_basket(browser, link):
-    page = ProductPage(browser, link)   
-    page.open()                
-    page.should_be_correct_adding_product_to_basket()
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        page = ProductPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        page = LoginPage(browser, link)
+		
+        email = str(time.time()) + "@fakemail.org"
+        password = "HaRd_password12345"
+				
+        page.register_new_user(email, password)
+        page.should_be_authorized_user()
+	
+    def test_user_cant_see_success_message(self, browser):
+        page = ProductPage(browser, link)   
+        page.open()               
+        page.should_not_be_success_message()
+    
+    def test_user_can_add_product_to_basket(self, browser):
+        page = ProductPage(browser, link)   
+        page.open()                
+        page.should_be_correct_adding_product_to_basket()
 	
 @pytest.mark.skip
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page = ProductPage(browser, link)   
     page.open()               
     page.add_product_to_basket()
-    page.should_not_be_success_message()
-	
-def test_guest_cant_see_success_message(browser):
-    page = ProductPage(browser, link)   
-    page.open()               
     page.should_not_be_success_message()
 	
 @pytest.mark.skip
